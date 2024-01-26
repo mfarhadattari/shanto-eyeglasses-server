c84ae0f1cda181b6a658a7b4746cd246aa01bf06
Mohammad Farhad <mfarhad.attari@gmail.com>
2024-01-26 09:57:48 +0600

    User Model Implemented

---
+++ src/app/modules/auth/auth.model.ts
@@ -0,0 +1,28 @@
+import { Schema, model } from 'mongoose';
+import { IUser } from './auth.interface';
+
+const userSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: 0,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = model<IUser>('User', userSchema);

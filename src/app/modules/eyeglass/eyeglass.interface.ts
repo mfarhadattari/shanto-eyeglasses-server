7790fd4ff9517c08d7dcc8b285c51bfe6e193f55
Mohammad Farhad <mfarhad.attari@gmail.com>
2024-01-26 21:54:45 +0600

    implemented eyeglass interface

---
+++ src/app/modules/eyeglass/eyeglass.interface.ts
@@ -0,0 +1,66 @@
+export type TFrameMaterial =
+  | 'metal'
+  | 'plastic'
+  | 'acetate'
+  | 'stainless-steel'
+  | 'wood'
+  | 'mixed-materials'
+  | 'other';
+
+export type TFrameShape =
+  | 'round'
+  | 'oval'
+  | 'square'
+  | 'rectangle'
+  | 'wayfarer'
+  | 'cat-eye'
+  | 'aviator'
+  | 'butterfly'
+  | 'round cat eye'
+  | 'semi-rimless'
+  | 'full rimless'
+  | 'browline'
+  | 'clubmaster'
+  | 'windsor'
+  | 'd-frame'
+  | 'pilot'
+  | 'octagon'
+  | 'hexagon'
+  | 'heart'
+  | 'pantoscopic'
+  | 'zyl'
+  | 'wraparound'
+  | 'other';
+
+export type TLensType =
+  | 'single-vision'
+  | 'bifocal'
+  | 'trifocal'
+  | 'progressive'
+  | 'digital'
+  | 'multifocal'
+  | 'polarized'
+  | 'photochromic'
+  | 'blue-cut'
+  | 'scratch-resistant'
+  | 'anti-reflective'
+  | 'uv-protective';
+
+export type TGender = 'men' | 'women' | 'unisex';
+
+export interface IEyeglass {
+  name: string;
+  price: number;
+  quantity: number;
+  frameMaterial: TFrameMaterial;
+  frameShape: TFrameShape;
+  lensType: TLensType;
+  brand: string;
+  gender: TGender;
+  color: string;
+  image: string;
+  otherRelevantAttributes: Record<string, unknown>;
+  isDeleted: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
